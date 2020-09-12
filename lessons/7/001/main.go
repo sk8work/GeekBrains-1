90b2031e6b9d4659d01941c9aa83673d7c23956a
sk8work <eshikhantsev@gmail.com>
2020-09-12 17:24:28 +0300

    less7

--- lessons/7/001/main.go
+++ lessons/7/001/main.go
@@ -3,38 +3,18 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
-	// fmt.Println(runtime.GOMAXPROCS(8))
-	// runtime.GOMAXPROCS(8)
-	// go func() {
-	// 	fmt.Print("Hello, World!")
-	// }()
-	// time.Sleep(time.Nanosecond)
-
-	// for i := 1; i < 7; i++ {
-	// 	go factorial(i)
-	// }
-	// fmt.Println("The End")
-
-	myOs := runtime.GOOS
-	switch myOs {
-	case "windows":
-		fmt.Println("WIN")
+	runtime.GOMAXPROCS(1)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
 	}
 
+	time.Sleep(500 * time.Millisecond)
 }
 
-// func factorial(n int) {
-// 	if n < 1 {
-// 		fmt.Println("Invalid input number")
-// 		return
-// 	}
-// 	result := 1
-// 	for i := 1; i <= n; i++ {
-// 		result *= i
-// 	}
-// 	fmt.Println(n, "-", result)
-
-// }
+// 0857224500
--- lessons/7/002/main.go
+++ lessons/7/002/main.go
@@ -2,41 +2,18 @@ package main
 
 import "fmt"
 
-type A struct {
-	arr []chan int
-}
-
-func (a *A) Start() {
-	a.arr = make([]chan int, 4)
-	for i := 0; i < 4; i++ {
-		a.arr[i] = make(chan int)
-	}
-	for i := 0; i < 4; i++ {
-		go func(i int) {
-			fmt.Println(<-a.arr[i])
-		}(i)
-
-	}
-}
-
-func (a *A) WriteTo(i int, elem int) {
-	a.arr[i] <- elem
-}
-
-func (a *A) ReSetup() {
-	a.arr = nil
-	a.Start()
-}
-
 func main() {
-	ch := make(chan struct{}, 10)
-	for i := 0; i < 10; i++ {
-		go func(a int) {
-			aa := &A{}
-			aa.Start()
-		}(i)
-	}
-	for i := 0; i < 10; i++ {
-		<-ch
-	}
+	ch := make(chan int, 10)
+	ch <- 1
+	ch <- 2
+	ch <- 3
+
+	fmt.Println(ch)
+	// &p := 0
+	p := <-ch
+	fmt.Println(p)
+	p = <-ch
+	fmt.Println(p)
+	p = <-ch
+	fmt.Println(p)
 }
